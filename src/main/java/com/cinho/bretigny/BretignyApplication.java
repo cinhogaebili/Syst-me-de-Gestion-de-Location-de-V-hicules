46c7f2bc9552e1d651762d9df99b4b494d39ffe6
cinhogaebili <cinhogaebili@gmail.com>
2024-04-07 01:00:36 +0200

    ajout des élements

---
+++ src/main/java/com/cinho/bretigny/BretignyApplication.java
@@ -0,0 +1,15 @@
+package com.cinho.bretigny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BretignyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BretignyApplication.class, args);
+
+		System.out.println("cinho");
+	}
+
+}
